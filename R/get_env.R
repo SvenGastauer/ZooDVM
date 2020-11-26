cc954564884c59b5aeff4e887ceb9cb3aab23061
Sven Gastauer <SvenGastauer@users.noreply.github.com>
2020-11-26 18:01:36 +0100

    get env - add lon lat

--- R/get_env.R
+++ R/get_env.R
@@ -4,10 +4,10 @@
 #' @param mission name or index number of the mission to be used
 #' @export
 #' @import  ncdf4
-#' @return dataframe contianing the temperature, salinity and fluorescence data per dive and depth interval
+#' @return dataframe contianing the lon, lat, time, temperature, salinity and fluorescence data per dive and depth interval
 #' @author Sven Gastauer
 #'
-get_env <- function(ncpath, mission){
+function(ncpath, mission){
   if (is.numeric(mission)){
     mission = get_mission(ncpath, ending='*.nc', patterns=NULL)[mission]
   }
@@ -16,10 +16,19 @@ get_env <- function(ncpath, mission){
   nc_data <- ncdf4::nc_open(paste0(ncpath, '/' ,mission))
 
   message(Sys.time(),': Getting GPS')
+
   t_end <- ncvar_get(nc_data, "gps/UTC_time_fix_end")
   ori = strsplit(ncatt_get(nc_data, "gps/UTC_time_fix_end")$units,'seconds since ')[[1]][2]
-  t_end = as.POSIXct(t_end,origin=ori)
+  t_end = as.POSIXct(t_end,origin=ori, tz= 'UTC')
   attr(t_end,'tzone') = 'UTC'
+
+  t_start <- ncvar_get(nc_data, "gps/UTC_time_fix_start")
+  ori = strsplit(ncatt_get(nc_data, "gps/UTC_time_fix_start")$units,'seconds since ')[[1]][2]
+  t_start = as.POSIXct(t_start,origin=ori, tz='UTC')
+  attr(t_start,'tzone') = 'UTC'
+
+  t_mid = t_start + (t_end - t_start)/2
+
 
   tmp <- ncvar_get(nc_data, "Environment/temperature")
   sal <- ncvar_get(nc_data, "Environment/salinity")
@@ -40,9 +49,38 @@ get_env <- function(ncpath, mission){
   temp <- temp[order(temp$Dive),]
   temp <- temp[,c('Dive','Depth','Depth_r','temp','sal','fluo')]
 
+  md = temp%>%group_by(Dive)%>%summarize(Dstart = max(Depth))
+  temp$Time = do.call('rbind',
+                      lapply(as.list(unique(temp$Dive)),
+                             FUN=function(i)data.frame(approx(c(md$Dstart[i],0),
+                                                              c(t_mid[i],t_end[i]),
+                                                              xout=temp$Depth[temp$Dive==unique(temp$Dive)[i]])$y)))
+  temp$Time = temp$Time[[1]]
+  temp$Time = as.POSIXct(temp$Time, origin='1970-01-01', tz='UTC')
+
+  Lon_start <- ncvar_get(nc_data, "gps/lon_start")
+  Lon_end <- ncvar_get(nc_data, "gps/lon_end")
+  temp$Lon = do.call('rbind',
+                     lapply(as.list(unique(temp$Dive)),
+                            FUN=function(i)data.frame(approx(c(as.numeric(t_start[i]),as.numeric(t_end[i])),
+                                                             c(Lon_start[i],Lon_end[i]),
+                                                             xout=as.numeric(temp$Time[temp$Dive==unique(temp$Dive)[i]]))$y)))
+
+  temp$Lon = temp$Lon[[1]]
+
+  Lat_start <- ncvar_get(nc_data, "gps/lat_start")
+  Lat_end <- ncvar_get(nc_data, "gps/lat_end")
+  temp$Lat = do.call('rbind',
+                     lapply(as.list(unique(temp$Dive)),
+                            FUN=function(i)data.frame(approx(c(as.numeric(t_start[i]),as.numeric(t_end[i])),
+                                                             c(Lat_start[i],Lat_end[i]),
+                                                             xout=as.numeric(temp$Time[temp$Dive==unique(temp$Dive)[i]]))$y)))
+
+  temp$Lat = temp$Lat[[1]]
+
+
   return(temp)
 }
-
 #' Plots the environmental data extracted from netCDF files
 #' @param data dataframe from get_env() function
 #' @param var name of the variable in data to be plotted, defaults to 'temp', options are 'temp', 'sal', 'fluo'
